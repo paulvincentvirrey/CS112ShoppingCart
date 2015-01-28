a51051c7216c5f48997c0f48265091ec02aa7cc3
Paul Vincent Virrey <paulvincentvirrey@gmail.com>
2015-01-28 20:00:02 +0800

    Compiled Assets2

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Shoppingcart
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # config.assets.enabled = false
+    config.assets.compile = true
   end
 end
